e789ba32f6f35a5a2415438af050eb601a5a6d4a
Carlo Ledesma <carlo.miguel.ledesma@gmail.com>
2015-10-31 11:34:01 +0800

    Removed all pyc recursively. Added user attributes

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    birthday = models.CharField(max_length=30)
+    contact_no = models.CharField(max_length=30)
